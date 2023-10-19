b9d0bb3ad030a783a3963d4f30bb6c2864e554ab
Hellnash <shubhamraj3417@gmail.com>
2023-10-19 15:59:37 +0530

    Securing with Oauth2 authentication and setting up authorization standards using OKTA

--- src/main/java/com/lcwd/RatingService/controllers/RatingController.java
+++ src/main/java/com/lcwd/RatingService/controllers/RatingController.java
@@ -5,6 +5,7 @@ import com.lcwd.RatingService.services.RatingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class RatingController {
     @Autowired
     RatingService ratingService;
 
+    @PreAuthorize("hasAuthority('Admin') hasAuthority('SCOPE_internal') || hasAuthority('Users')") //anyone can create a rating
     @PostMapping
     public ResponseEntity<Rating> createRating(@RequestBody Rating rating){
         Rating savedRating = ratingService.createRating(rating);
@@ -24,6 +26,7 @@ public class RatingController {
 
     }
 
+    @PreAuthorize("hasAuthority('Admin') hasAuthority('SCOPE_internal') || hasAuthority('Users')") //anyone can check all ratings
     @GetMapping
     public ResponseEntity<List<Rating>> getAllRatings(){
         List<Rating> allRatings = ratingService.getAllRatings();
@@ -31,6 +34,7 @@ public class RatingController {
 
     }
 
+    @PreAuthorize("hasAuthority('Admin') hasAuthority('SCOPE_internal') || hasAuthority('Users')") //anyone can check all ratings of a hotel
     @GetMapping("/hotel/{hotelId}")
     public ResponseEntity<List<Rating>> getRatingOfHotel(@PathVariable String hotelId){
         List<Rating> hotelRatings = ratingService.getAllRatingOfHotel(hotelId);
@@ -38,6 +42,7 @@ public class RatingController {
 
     }
 
+    @PreAuthorize("hasAuthority('Admin') hasAuthority('SCOPE_internal') || hasAuthority('Users')") //anyone can check all the ratings done by the user
     @GetMapping("/user/{userId}")
     public ResponseEntity<List<Rating>> getRatingOfUser(@PathVariable String userId){
         List<Rating> userRatings = ratingService.getAllRatingOfUser(userId);
